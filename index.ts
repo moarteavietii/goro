8223c2f2edb0d8e4d7b443d2e463c7f8626a8eb6
moarteavietii <cold.freezy+github@gmail.com>
2024-09-01 10:48:05 +0300

    add deploy endpoint

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
-const express = require('express')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const express = require('express');
 const app = express()
 const port = 9615
 
 app.get('/', (req, res) => {
   res.send('Hello there')
 })
+
+app.post('/deploy', (req, res) => {
+  exec('git pull origin master').then((resolve, reject) => {
+    if(reject) {
+      console.error(reject);
+      res.status(500);
+      res.send('Error');
+    } else {
+        res.send('Success');
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
